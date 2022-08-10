a14a4984a6380f2fd1118df76a75b5611b85c2c7
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-08-10 09:24:17 +0700

    Update wserver.py

--- web/wserver.py
+++ web/wserver.py
@@ -776,9 +776,9 @@ def set_priority(id_):
 def homepage():
     return "<h1>See mirror-leech-telegram-bot <a href='https://www.github.com/anasty17/mirror-leech-telegram-bot'>@GitHub</a> By <a href='https://github.com/anasty17'>Anas</a></h1>"
 
-@app.errorhandler(Exception)
-def page_not_found(e):
-    return f"<h1>404: Torrent not found! Mostly wrong input. <br><br>Error: {e}</h2>", 404
+# @app.errorhandler(Exception)
+# def page_not_found(e):
+#    return f"<h1>404: Torrent not found! Mostly wrong input. <br><br>Error: {e}</h2>", 404
 
 if __name__ == "__main__":
     app.run()
